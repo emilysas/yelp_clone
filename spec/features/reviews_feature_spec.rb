c8bd66e85536d9a6c98560e61c5c1780fb1fd63a
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-17 09:23:39 +0000

    passing test - reviews deleted if restaurant deleted

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -14,5 +14,15 @@ feature 'reviewing' do
     expect(current_path).to eq('/restaurants')
   end
 
+  scenario 'if a user deletes a restaurant, the reviews will also be deleted' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: "Rating"
+    click_button "Leave Review"
+    click_link "Delete KFC"
+    expect(page).not_to have_content "so so"
+    expect(current_path).to eq('/restaurants')
+  end
   
 end
